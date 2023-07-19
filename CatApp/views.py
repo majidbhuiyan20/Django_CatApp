71b6aace6cc71793c6f481a04b99e81da48af7fa
Majid Bhuiyan <majid.bhuiyan2011@gmail.com>
2023-07-19 10:10:57 +0600

    Create Serializer

--- CatApp/views.py
+++ CatApp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import CatShop
+from .serializers import CatShopSerializer
 
 # Create your views here.
+class CatShopList(generics.ListCreateAPIView):
+    queryset = CatShop.objects.all()
+    serializer_class = CatShopSerializer
+
+class CatShopDetails(generics.RetrieveUpdateDestroyAPIView):
+    queryset = CatShop.objects.all()
+    serializer_class = CatShopSerializer
